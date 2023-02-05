04dfb27eb3a10a9f59a53ec6451b15ca5bd7abee
Batug Kocak <batugkocak@gmail.com>
2023-02-05 10:47:47 +0300

    first commit

---
+++ lib/Page/login_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      body: SafeArea(
+        child: Column(
+          children: const [
+            //Logo
+
+            //Quote
+
+            //Username Texfield
+
+            //Password Textfield
+
+            //Forgot Password?
+
+            //SignIn Button
+
+            //or continue with
+
+            //Google Button
+
+            //Apple Button
+
+            //Not a member? Sign Up
+          ],
+        ),
+      ),
+    );
+  }
+}

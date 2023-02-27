11df0c2804284d369e3577162060c0002107ccce
Batug Kocak <batugkocak@gmail.com>
2023-02-27 11:37:01 +0300

    fix pixel overflow

--- lib/Page/login_page.dart
+++ lib/Page/login_page.dart
@@ -26,130 +26,132 @@ class LoginPage extends StatelessWidget {
         child: Center(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: screenWidth / 20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: screenHeight / 10,
-                ),
-                //Logo
-                const Icon(Icons.account_balance_sharp, size: 100.0),
-                SizedBox(
-                  height: screenHeight / 20,
-                ),
-                //Quote
-                Text(
-                  "Welcome back! You have been missed.",
-                  style: TextStyle(
-                    color: Colors.grey[800],
-                    fontSize: 16,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: screenHeight / 10,
                   ),
-                ),
-
-                SizedBox(
-                  height: screenHeight / 40,
-                ),
-                //Username Texfield
-                MyTextField(
-                    controller: usernameController,
-                    hintText: "Username",
-                    obscureText: false),
-                SizedBox(
-                  height: screenHeight / 40,
-                ),
-                //Password Textfield
-                MyTextField(
-                    controller: passwordController,
-                    hintText: "Password",
-                    obscureText: true),
-
-                SizedBox(
-                  height: screenHeight / 100,
-                ),
-                //Forgot Password?
-                Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                  //Logo
+                  const Icon(Icons.account_balance_sharp, size: 100.0),
+                  SizedBox(
+                    height: screenHeight / 20,
+                  ),
+                  //Quote
                   Text(
-                    "Forgot Password?",
-                    style: TextStyle(color: Colors.grey.shade700),
+                    "Welcome back! You have been missed.",
+                    style: TextStyle(
+                      color: Colors.grey[800],
+                      fontSize: 16,
+                    ),
                   ),
-                ]),
-                SizedBox(
-                  height: screenHeight / 100,
-                ),
-                //SignIn Button
-                MyButton("Sign In", signUserIn),
 
-                SizedBox(
-                  height: screenHeight / 100,
-                ),
-                //or continue with
-                Padding(
-                  padding: const EdgeInsets.all(5),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Expanded(
-                        child: Divider(
-                          thickness: 3,
+                  SizedBox(
+                    height: screenHeight / 40,
+                  ),
+                  //Username Texfield
+                  MyTextField(
+                      controller: usernameController,
+                      hintText: "Username",
+                      obscureText: false),
+                  SizedBox(
+                    height: screenHeight / 40,
+                  ),
+                  //Password Textfield
+                  MyTextField(
+                      controller: passwordController,
+                      hintText: "Password",
+                      obscureText: true),
+
+                  SizedBox(
+                    height: screenHeight / 100,
+                  ),
+                  //Forgot Password?
+                  Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                    Text(
+                      "Forgot Password?",
+                      style: TextStyle(color: Colors.grey.shade700),
+                    ),
+                  ]),
+                  SizedBox(
+                    height: screenHeight / 100,
+                  ),
+                  //SignIn Button
+                  MyButton("Sign In", signUserIn),
+
+                  SizedBox(
+                    height: screenHeight / 100,
+                  ),
+                  //or continue with
+                  Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Expanded(
+                          child: Divider(
+                            thickness: 3,
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: Text(
-                          "Or continue with",
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Text(
+                            "Or continue with",
+                            style: TextStyle(color: Colors.grey.shade700),
+                          ),
+                        ),
+                        const Expanded(
+                          child: Divider(
+                            thickness: 3,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  SizedBox(
+                    height: screenHeight / 20,
+                  ),
+
+                  //Google/ Apple Button
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: const [
+                      SquareTile(imagePath: "lib/Image/google.png"),
+                      SquareTile(imagePath: "lib/Image/apple.png"),
+                    ],
+                  ),
+
+                  SizedBox(
+                    height: screenHeight / 15,
+                  ),
+
+                  //Not a member? Sign Up
+
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignInPage()),
+                      );
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Not a member? ",
                           style: TextStyle(color: Colors.grey.shade700),
                         ),
-                      ),
-                      const Expanded(
-                        child: Divider(
-                          thickness: 3,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-
-                SizedBox(
-                  height: screenHeight / 20,
-                ),
-
-                //Google/ Apple Button
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: const [
-                    SquareTile(imagePath: "lib/Image/google.png"),
-                    SquareTile(imagePath: "lib/Image/apple.png"),
-                  ],
-                ),
-
-                SizedBox(
-                  height: screenHeight / 15,
-                ),
-
-                //Not a member? Sign Up
-
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SignInPage()),
-                    );
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Not a member? ",
-                        style: TextStyle(color: Colors.grey.shade700),
-                      ),
-                      const Text(
-                        "Register Now!",
-                        style: TextStyle(color: Colors.blueAccent),
-                      )
-                    ],
-                  ),
-                )
-              ],
+                        const Text(
+                          "Register Now!",
+                          style: TextStyle(color: Colors.blueAccent),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
--- lib/Page/register_page.dart
+++ lib/Page/register_page.dart
@@ -28,61 +28,63 @@ class SignInPage extends StatelessWidget {
           child: Padding(
         padding: EdgeInsets.symmetric(horizontal: screenWidth / 20),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Icon(
-                CupertinoIcons.signature,
-                size: 120,
-              ),
-              SizedBox(
-                height: screenHeight / 20,
-              ),
-              Text(
-                "Welcome! It's nice to see you here.",
-                style: TextStyle(
-                  color: Colors.grey[800],
-                  fontSize: 16,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  CupertinoIcons.signature,
+                  size: 120,
                 ),
-              ),
-              SizedBox(
-                height: screenHeight / 40,
-              ),
-              MyTextField(
-                  controller: usernameController,
-                  hintText: "Username",
-                  obscureText: false),
-              SizedBox(
-                height: screenHeight / 40,
-              ),
-              MyTextField(
-                  controller: emailController,
-                  hintText: "Email",
-                  obscureText: false),
-              SizedBox(
-                height: screenHeight / 40,
-              ),
-              MyTextField(
-                  controller: passwordController,
-                  hintText: "Password",
-                  obscureText: true),
-              SizedBox(
-                height: screenHeight / 40,
-              ),
-              MyTextField(
-                  controller: cnfpasswordController,
-                  hintText: "Repeat Password",
-                  obscureText: true),
+                SizedBox(
+                  height: screenHeight / 20,
+                ),
+                Text(
+                  "Welcome! It's nice to see you here.",
+                  style: TextStyle(
+                    color: Colors.grey[800],
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight / 40,
+                ),
+                MyTextField(
+                    controller: usernameController,
+                    hintText: "Username",
+                    obscureText: false),
+                SizedBox(
+                  height: screenHeight / 40,
+                ),
+                MyTextField(
+                    controller: emailController,
+                    hintText: "Email",
+                    obscureText: false),
+                SizedBox(
+                  height: screenHeight / 40,
+                ),
+                MyTextField(
+                    controller: passwordController,
+                    hintText: "Password",
+                    obscureText: true),
+                SizedBox(
+                  height: screenHeight / 40,
+                ),
+                MyTextField(
+                    controller: cnfpasswordController,
+                    hintText: "Repeat Password",
+                    obscureText: true),
 
-              SizedBox(
-                height: screenHeight / 100,
-              ),
-              //SignIn Button
-              MyButton(
-                "Sign Up",
-                (() {}),
-              )
-            ],
+                SizedBox(
+                  height: screenHeight / 100,
+                ),
+                //SignIn Button
+                MyButton(
+                  "Sign Up",
+                  (() {}),
+                )
+              ],
+            ),
           ),
         ),
       )),
